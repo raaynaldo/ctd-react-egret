7b2fc9f9df9be353e252bfec5d2f205920592ea2
Raynaldo Sutisna <raaynaldo@gmail.com>
2021-08-29 21:12:10 -0400

    fix: bug

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Todo List</h1>
-        <TodoList />
-        <AddTodoForm />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <TodoList />
+      <AddTodoForm />
+    </div>
+  );
 }
-
-// function App() {
-
-//   return (
-//     <div>
-//       <h1>Todo List</h1>
-//       <TodoList />
-//       <AddTodoForm />
-//     </div>
-//   );
-// }
 
 export default App;
