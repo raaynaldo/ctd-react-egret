91128513292d60bf35da439486c0825a78102070
Raynaldo Sutisna <raaynaldo@gmail.com>
2021-10-11 00:02:04 -0400

    feat: lesson 1-7 finish

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,22 +3,47 @@ import AddTodoForm from './AddTodoForm';
 // import ReactFlow from './ReactFlow';
 import TodoList from './TodoList';
 
-function useSemiPersistentState() {
-  const [todoList, setTodoList] = useState(
-    localStorage.getItem('savedTodoList')
-      ? JSON.parse(localStorage.getItem('savedTodoList'))
-      : []
-  );
+// function useSemiPersistentState() {
+//   const [todoList, setTodoList] = useState(
+//     localStorage.getItem('savedTodoList')
+//       ? JSON.parse(localStorage.getItem('savedTodoList'))
+//       : []
+//   );
 
-  useEffect(() => {
-    localStorage.setItem('savedTodoList', JSON.stringify(todoList));
-  }, [todoList]);
+//   useEffect(() => {
+//     localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+//   }, [todoList]);
 
-  return [todoList, setTodoList];
-}
+//   return [todoList, setTodoList];
+// }
 
 function App() {
-  const [todoList, setTodoList] = useSemiPersistentState();
+  // const [todoList, setTodoList] = useSemiPersistentState();
+  const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          data: {
+            todoList: localStorage.getItem('savedTodoList')
+              ? JSON.parse(localStorage.getItem('savedTodoList'))
+              : [],
+          },
+        });
+      }, 2000);
+    }).then((result) => {
+      setIsLoading(false);
+      setTodoList(result.data.todoList);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!isLoading) {
+      localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+    }
+  }, [todoList, isLoading]);
 
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo]);
@@ -33,7 +58,11 @@ function App() {
     <>
       <h1>Todo List</h1>
       <AddTodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+      )}
       {/* <ReactFlow /> */}
     </>
   );
